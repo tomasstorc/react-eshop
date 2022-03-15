79b9840f16a793bc1ab68d87a294436981206ee7
Tomas Storc <tomas.storc@gmail.com>
2022-03-15 09:41:24 +0100

    homepage section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return <div>Hello world</div>;
+  return <HomePage />;
 }
 
 export default App;
---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../componenets/directory/directory.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
