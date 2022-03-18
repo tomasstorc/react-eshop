82c88b0f501f2dff45be16a7c18e3b3ca95ab610
Tomas Storc <tomas.storc@gmail.com>
2022-03-18 08:23:53 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./pages/navigation/navigation.component";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<HomePage />} />
+          <Route path="shop" element={<h1>TEST</h1>} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Logo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
