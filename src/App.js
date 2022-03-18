fd5381127c952b7c99c1fdf6ef5c95d8da203644
Tomas Storc <tomas.storc@gmail.com>
2022-03-18 08:31:11 +0100

    added sign in component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./pages/navigation/navigation.component";
+import SignIn from "./pages/sign-in/sign-in.component";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Navigation />}>
           <Route index element={<HomePage />} />
           <Route path="shop" element={<h1>TEST</h1>} />
+          <Route path="sign-in" element={<SignIn />} />
         </Route>
       </Routes>
     </div>
---
+++ src/pages/sign-in/sign-in.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1>I am the sign in page</h1>
+    </div>
+  );
+};
+
+export default SignIn;
