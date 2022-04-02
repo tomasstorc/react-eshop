01b6c70e88522e893482150bb2ed2a09e9542e18
Tomas Storc <tomas.storc@gmail.com>
2022-04-02 17:21:22 +0200

    poducts context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./pages/navigation/navigation.component";
 import Authentication from "./pages/authentication/authentication.component";
+import Shop from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<HomePage />} />
-          <Route path="shop" element={<h1>TEST</h1>} />
+          <Route path="shop" element={<Shop />} />
           <Route path="auth" element={<Authentication />} />
         </Route>
       </Routes>
